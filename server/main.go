d4888113406304c97ef382fc9d187bd3fc4fd2d7
tong <492065988@qq.com>
2023-02-04 16:36:21 +0800

    1

--- server/main.go
+++ server/main.go
@@ -30,7 +30,7 @@ func main() {
 	initialize.DBList()
 	if global.GVA_DB != nil {
 		initialize.RegisterTables(global.GVA_DB) // 初始化表
-		// 程序结束前关闭数据库链接
+		// 程序结束前关闭数据库链接1
 		db, _ := global.GVA_DB.DB()
 		defer db.Close()
 	}
